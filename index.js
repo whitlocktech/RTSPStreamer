e9f2a94ef44e5f38f0144a737179e701b370eb74
whitlocktech <whitlocktech@gmail.com>
2023-06-17 00:14:47 -0500

    update to use streamurl and key

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
-const { google } = require('googleapis');
 const ffmpeg = require('fluent-ffmpeg');
 
 class RTSPStreamer {
   constructor(options) {
     this.rtspStreamUrl = options.rtspStreamUrl;
-    this.youtubeStreamKey = options.youtubeStreamKey;
-    this.youtubeClientId = options.youtubeClientId;
-    this.youtubeClientSecret = options.youtubeClientSecret;
-    this.youtubeRefreshToken = options.youtubeRefreshToken;
+    this.youtubeStreamKey = options.streamKey;
+    this.youtubeStreamUrl = options.streamUrl;
     this.title = options.title || 'RTSP Stream';
-    this.privacyStatus = options.privacyStatus || 'public';
   }
 
   async start() {
-    const oAuth2Client = new google.auth.OAuth2(
-      this.youtubeClientId,
-      this.youtubeClientSecret
-    );
-    oAuth2Client.setCredentials({ refresh_token: this.youtubeRefreshToken });
-
-    const youtube = google.youtube({
-      version: 'v3',
-      auth: oAuth2Client,
-    });
-
-    const broadcast = await youtube.liveBroadcasts.insert({
-      part: ['snippet', 'status'],
-      requestBody: {
-        snippet: {
-          title: this.title,
-          scheduledStartTime: new Date(),
-        },
-        status: {
-          privacyStatus: this.privacyStatus,
-        },
-      },
-    });
-
-    const broadcastId = broadcast.data.id;
-    const streamKey = await youtube.liveStreams.insert({
-      part: ['snippet', 'cdn'],
-      requestBody: {
-        snippet: {
-          title: this.title,
-        },
-        cdn: {
-          format: '720p',
-          ingestionType: 'rtmp',
-        },
-      },
-    }).then((stream) => stream.data.cdn.ingestionInfo.streamName);
-
     ffmpeg(this.rtspStreamUrl)
       .inputOptions('-rtsp_transport tcp')
       .inputFormat('rtsp')
-      .output(`rtmp://a.rtmp.youtube.com/live2/${streamKey}`)
+      .output(`${this.streamUrl}/${this.streamKey}`)
       .outputOptions('-c:v copy', '-c:a copy')
       .on('start', () => console.log('Start streaming'))
       .on('error', (err) => console.log('Error:', err.message))
